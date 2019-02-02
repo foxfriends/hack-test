b28fbb8e35eadf48e7987b2e7f9be020c7b06841
Cameron Eldridge <cameldridge+git@gmail.com>
2019-02-01 19:56:18 -0500

    REmove try

--- lb.php
+++ lb.php
@@ -1,62 +1,52 @@
 <?php
-try {
-  $file = fopen('lb.txt', 'r');
-  $scores = fread($file, filesize('lb.txt'));
+$file = fopen('lb.txt', 'r');
+$scores = fread($file, filesize('lb.txt'));
+fclose($file);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $level = intval($_GET['level']);
+  $hash = $_GET['hash'];
+  $nextLevel = $level + 1;
+
+  // ensure they actually are on a level
+  if (!file_exists("$nextLevel/$hash.html")) {
+    echo "Aborted";
+    exit;
+  }
+
+  $id = $_GET['id'];
+  $name = $_GET['name'];
+  $attempts = intval($_GET['attempts']);
+
+  $lines = explode("\n", $scores);
+  $added = false;
+  foreach ($lines as &$line) {
+    $list = explode(',', $line);
+    if ($list[0] == $id) {
+      if (intval($list[1]) < $level) {
+        $list[0] = $name;
+        $list[1] = $level;
+        $list[2] = intval($list[2]) + $attempts;
+        $line = join(',', $list);
+      }
+      $added = true;
+      break;
+    }
+  }
+  unset($line);
+
+  if (!$added) {
+    array_push($lines, "$id,$name,$level,$attempts");
+  }
+
+  $output = join("\n", $lines);
+
+  $file = fopen('lb.txt', 'w');
+  fwrite($file, $output);
   fclose($file);
 
-  echo $_SERVER['REQUEST_METHOD'];
-
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $level = intval($_GET['level']);
-    $hash = $_GET['hash'];
-    $nextLevel = $level + 1;
-
-    echo $level, $hash, "\n";
-    echo $nextLevel, "\n";
-    echo file_exists("$nextLevel/$hash.html");
-
-    // ensure they actually are on a level
-    if (!file_exists("$nextLevel/$hash.html")) {
-      echo "Aborted";
-      exit;
-    }
-
-    $id = $_GET['id'];
-    $name = $_GET['name'];
-    $attempts = intval($_GET['attempts']);
-
-    $lines = explode("\n", $scores);
-    $added = false;
-    foreach ($lines as &$line) {
-      $list = explode(',', $line);
-      if ($list[0] == $id) {
-        if (intval($list[1]) < $level) {
-          $list[0] = $name;
-          $list[1] = $level;
-          $list[2] = intval($list[2]) + $attempts;
-          $line = join(',', $list);
-        }
-        $added = true;
-        break;
-      }
-    }
-    unset($line);
-
-    if (!$added) {
-      array_push($lines, "$id,$name,$level,$attempts");
-    }
-
-    $output = join("\n", $lines);
-
-    $file = fopen('lb.txt', 'w');
-    fwrite($file, $output);
-    fclose($file);
-
-    echo "OK!"
-  } else {
-    echo $scores;
-  }
-} catch (Exception $exception) {
-  echo "Error: " . $exception->getMessage() . "\n";
+  echo "OK!"
+} else {
+  echo $scores;
 }
 ?>
